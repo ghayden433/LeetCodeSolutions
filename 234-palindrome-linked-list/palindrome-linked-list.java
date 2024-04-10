e257dc97bdaacb778c3715fcfb5e845c71a95990
Hayden Gillen <63254760+ghayden433@users.noreply.github.com>
2024-04-10 11:07:43 -0400

    Time: 4 ms (82.38%) | Memory: 68 MB (48.93%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -10,18 +10,41 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        String obv = "";
+        ListNode slow = head;
+        ListNode fast = head;
+        if (head == null || head.next == null) return true;
 
-        while (head != null){
-            obv = obv + head.val;
+
+
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+
+        ListNode prev = null;
+        ListNode curr = slow;
+        ListNode next = null;
+
+
+        while (curr != null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        ListNode revHead = prev;
+
+
+        while (revHead != null){
+            if (revHead.val != head.val) return false;
+            revHead = revHead.next;
             head = head.next;
         }
 
-        int length = obv.length();
-        for (int i = 0; i < (length / 2); i++){
-            if (obv.charAt(i) != obv.charAt(length - 1 - i)) return false;
-        }
-        
+
         return true;
     }
 }
+
