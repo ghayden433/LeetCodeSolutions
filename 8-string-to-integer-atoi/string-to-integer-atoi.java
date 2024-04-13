a3d1e9eaf904a258a0d074c8d108dac73c298867
Hayden Gillen <63254760+ghayden433@users.noreply.github.com>
2024-04-13 16:01:21 -0400

    Time: 11 ms (5.14%) | Memory: 42.7 MB (17.12%) - LeetSync

---
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int myAtoi(String s) {
+        String result = "";
+        int i = 0;
+        int resultInt = 0;
+
+
+        while (i < s.length() && s.charAt(i) == ' ') i++;
+        if (i < s.length() && (s.charAt(i) == '-' || s.charAt(i) == '+')) {
+            if (s.charAt(i) == '-'){
+                result += "-";
+            }
+            i++;
+        }
+        result += "0";
+
+        while (i < s.length() && Character.isDigit(s.charAt(i))){
+            result += s.charAt(i);
+            i++;
+        }
+
+        try {
+            return (int)Integer.parseInt(result);
+        } catch(NumberFormatException e){
+            i = result.compareTo("0") > 0? 2147483647: -2147483648;
+        }
+        return i;
+    }
+}
