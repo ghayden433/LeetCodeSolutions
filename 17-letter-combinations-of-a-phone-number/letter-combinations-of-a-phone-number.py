627c8f1ba46679b2eddd690a7cadb5847eec8f85
Hayden Gillen <63254760+ghayden433@users.noreply.github.com>
2024-06-05 16:50:03 -0400

    Time: 4 ms (98.78%) | Memory: 11.7 MB (69.80%) - LeetSync

---
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        if digits == "":
+            return {}
+        result = list()
+        pointers = [0] * len(digits)
+        letters = ["", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"]
+        while pointers[0] < len(letters[int(digits[0])]):
+            temp = ""
+            for i in range(len(digits)):
+                temp = temp + letters[int(digits[i])][pointers[i]]
+            result.append(temp)
+            pointers[len(pointers) - 1] += 1
+            for i in range(len(pointers) - 1, 0, -1):
+                if pointers[i] >= len(letters[int(digits[i])]):
+                    pointers[i - 1] += 1
+                    pointers[i] = 0
+        return result
