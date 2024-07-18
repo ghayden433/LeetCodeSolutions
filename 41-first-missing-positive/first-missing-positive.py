ad7760db24bbd3c49a02e95219d5a1aa1f9477c3
Hayden Gillen <63254760+ghayden433@users.noreply.github.com>
2024-07-18 13:44:13 -0400

    Time: 360 ms (83.78%) | Memory: 28.6 MB (11.77%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.py
+++ 41-first-missing-positive/first-missing-positive.py
@@ -5,9 +5,16 @@ class Solution(object):
         :rtype: int
         """
         result = set()
+        minnum = 2000000000
         for item in nums:
             result.add(item)
+            if item < minnum and item > 0:
+                minnum = item
         
+        if minnum != 1:
+            return 1
+
+
         i = 1
         while True:
             if i not in result:
