dd255c419db308b24ffd24b4e496ccf52d58c54f
Hayden Gillen <63254760+ghayden433@users.noreply.github.com>
2025-01-10 15:33:26 -0500

    Time: 0 ms (100.00%) | Memory: 45.5 MB (90.32%) - LeetSync

---
+++ 792-binary-search/binary-search.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        int middle;
+        while (low <= high) {
+            middle = low + ((high-low) /2);
+
+            if (nums[middle] == target) {
+                return middle;
+            }
+
+            if (nums[middle] < target) {
+                low = middle + 1;
+            }
+
+            if (nums[middle] > target) {
+                high = middle - 1;
+            }
+        }
+        return -1;
+    }
+}
