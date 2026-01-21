04a6bb3bc93e0b4201d0148cc24711ca780c8b57
Hayden Gillen <63254760+ghayden433@users.noreply.github.com>
2026-01-21 18:19:53 -0500

    Time: 3 ms (32.18%) | Memory: 11.7 MB (42.20%) - LeetSync

---
+++ 412-fizz-buzz/fizz-buzz.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> resultVec;
+        for(int i = 1; i <= n; i++){
+            string result = "";
+            if (i % 3 == 0) result += "Fizz";
+            if (i % 5 == 0) result += "Buzz";
+            if (result == "") result += to_string(i);
+            resultVec.push_back(result);
+        }
+        return resultVec;
+    }
+};
